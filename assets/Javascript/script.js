7f09552fb45e855fd6a8fe539cba2f8ba56390d8
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2022-09-15 23:28:38 -0500

    Add files via upload

---
+++ assets/Javascript/script.js
@@ -0,0 +1,5 @@
+var APIKey = "9744be1fb817e05df45b93dbee033bc4";
+var city;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
+
+// fetch(queryURL)
