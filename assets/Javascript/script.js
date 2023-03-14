1d65b5f26420aca7514bcaf56af26af89fcf2699
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-13 21:06:22 -0500

    Added some notes to file

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -2,7 +2,12 @@ var APIKey = "9744be1fb817e05df45b93dbee033bc4";
 var city;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
 
-// fetch(queryURL)
+//Need to have a saved search history & ability to remove search history
+//Today's forcast
+//5-day forcast
+
+
+
 function getApi() {
     fetch(queryURL)
     .then(function (response) {
