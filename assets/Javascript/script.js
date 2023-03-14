2f7a107c66b1e16f88c73b8b8cbcdb5a10490159
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-13 20:49:23 -0500

    Working on JS to fetch the api

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -3,3 +3,17 @@ var city;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
 
 // fetch(queryURL)
+function getApi() {
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (var i = 0; i < data.length; i++) {
+            console.log (data);
+        }
+
+    })
+}
+
+fetchButton.addEventListener('click', getApi);
