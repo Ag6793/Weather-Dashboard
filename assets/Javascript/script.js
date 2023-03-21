3d784186eba6ca424150efae7b4e1c6e0b0a9458
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-20 23:15:52 -0500

    Working on js

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,7 +1,9 @@
 var APIKey = "9744be1fb817e05df45b93dbee033bc4";
-var city;
+// var city;
 var requestURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
-
+var fetchButton = document.getElementById('fetch-button');
+var todayForcast = document.getElementById('todayForcast'); //Today's forcast div body
+var fiveDayForcast = document.getElementById('5DayForcast'); //5 Day forcast div body
 //Need to have a saved search history & ability to remove search history
 //Today's forcast
 //5-day forcast
@@ -16,6 +18,9 @@ function getApi() {
     .then(function (data) {
         for (var i = 0; i < data.length; i++) {
             console.log (data);
+            var textItem = document.createElement('p');
+            textItem.textContent = data[i].html_url;
+            todayForcast.appendChild(textItem);
         }
 
     })
