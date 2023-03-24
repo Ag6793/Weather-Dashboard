47083d1ab0d4ff23df150134eac4b05408cf38e8
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-23 23:57:42 -0500

    Working on adding js script to make the app functional

---
+++ assets/Javascript/display-search.js
@@ -0,0 +1,5 @@
+// This is how the results will be displayed
+// var resultTextEl = document.querySelector('#result-text');
+var todayResultContentEl = document.querySelector('#today-result-content');
+var fiveDayResultContentEl = document.querySelector('#fiveday-result-content');
+var searchFormEl = document.querySelector('#search-form');
--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,29 +1,28 @@
-var APIKey = "9744be1fb817e05df45b93dbee033bc4";
-// var city;
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
-var fetchButton = document.getElementById('fetch-button');
-var todayForcast = document.getElementById('todayForcast'); //Today's forcast div body
-var fiveDayForcast = document.getElementById('5DayForcast'); //5 Day forcast div body
-//Need to have a saved search history & ability to remove search history
-//Today's forcast
-//5-day forcast
+// var APIKey = "9744be1fb817e05df45b93dbee033bc4";
+
+// var requestURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
+// var fetchButton = document.getElementById('fetch-button');
+// var todayForcast = document.getElementById('todayForcast'); //Today's forcast div body
+// var fiveDayForcast = document.getElementById('5DayForcast'); //5 Day forcast div body
 
 
+var searchFormEl = document.querySelector('#search-form');
 
-function getApi() {
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            console.log (data);
-            var textItem = document.createElement('p');
-            textItem.textContent = data[i].html_url;
-            todayForcast.appendChild(textItem);
-        }
-
-    })
-}
-
-fetchButton.addEventListener('click', getApi);
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    // var formatInputVal = document.querySelector('#format-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  
+    location.assign(queryString);
+  }
+  
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+  
