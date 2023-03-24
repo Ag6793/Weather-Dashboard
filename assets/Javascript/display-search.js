997862cfcc91f6206ecacd9dfa6f0aa5ad55b39d
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-24 00:21:25 -0500

    Added script to js file

--- assets/Javascript/display-search.js
+++
@@ -1,5 +0,0 @@
-// This is how the results will be displayed
-// var resultTextEl = document.querySelector('#result-text');
-var todayResultContentEl = document.querySelector('#today-result-content');
-var fiveDayResultContentEl = document.querySelector('#fiveday-result-content');
-var searchFormEl = document.querySelector('#search-form');
--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,28 +1,30 @@
-// var APIKey = "9744be1fb817e05df45b93dbee033bc4";
+var APIKey = "46b2f5e4840b0ff2146a6e57e65a86d2";
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var fetchButton = document.getElementById('fetch-button');
 
-// var requestURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";//for user input
-// var fetchButton = document.getElementById('fetch-button');
-// var todayForcast = document.getElementById('todayForcast'); //Today's forcast div body
-// var fiveDayForcast = document.getElementById('5DayForcast'); //5 Day forcast div body
+var todayResultContentEl = document.querySelector('#today-result-content');
+var fiveDayResultContentEl = document.querySelector('#fiveday-result-content');
+var searchFormEl = document.querySelector('#search-form');
 
+//Need to add local storage to store any persistent data
 
 var searchFormEl = document.querySelector('#search-form');
 
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-  
-    var searchInputVal = document.querySelector('#search-input').value;
-    // var formatInputVal = document.querySelector('#format-input').value;
-  
-    if (!searchInputVal) {
-      console.error('You need a search input value!');
-      return;
-    }
-  
-    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-  
-    location.assign(queryString);
-  }
-  
-  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+function getApi() {
+  fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
+
   
