c59b1bafc5fbb27962aba916d3eb02f2de74e353
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-26 23:38:08 -0500

    Updating js elements

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -2,12 +2,19 @@ var APIKey = "46b2f5e4840b0ff2146a6e57e65a86d2";
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var fetchButton = document.getElementById('fetch-button');
+var cityNameEl = document.getElementById('cityName');
+var tempEl = document.getElementById('temp');
+var windEl = document.getElementById('wind');
+var humidityEl = document.getElementById('humidity');
+var uvIndexEl = document.getElementById('uvIndex');
+// var dateEl = document.getElementById('')  <= Needs today's date displayed and 5 days out displayed as well
 
 var todayResultContentEl = document.querySelector('#today-result-content');
 var fiveDayResultContentEl = document.querySelector('#fiveday-result-content');
 var searchFormEl = document.querySelector('#search-form');
 
 //Need to add local storage to store any persistent data
+
 
 var searchFormEl = document.querySelector('#search-form');
 
