bed3df7ef9d78a2be234e7199a3e7fc64f3167bb
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-27 23:24:32 -0500

    Added event listener script

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,19 +1,3 @@
-// var apiKey = "46b2f5e4840b0ff2146a6e57e65a86d2";
-// var city;
-// //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-// var fetchButton = document.getElementById('fetch-button');
-// var cityNameEl = document.getElementById('cityName');
-// var tempEl = document.getElementById('temp');
-// var windEl = document.getElementById('wind');
-// var humidityEl = document.getElementById('humidity');
-// // var dateEl = document.getElementById('')  <= Needs today's date displayed and 5 days out displayed as well
-
-// var todayResultContentEl = document.querySelector('#today-result-content');
-// var fiveDayResultContentEl = document.querySelector('#fiveday-result-content');
-// var searchFormEl = document.querySelector('#search-form');
-
-//Need to add local storage to store any persistent data
-
 let weather = {
   apiKey: "46b2f5e4840b0ff2146a6e57e65a86d2",
   fetchWeather: function (city) {
@@ -35,8 +19,21 @@ let weather = {
     document.querySelector("#temp").innerText = "Temp: " + temp + "॰F";
     document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector("#speed").innerText = "Wind Speed: " + speed + "mph";
-
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector("#search-input").value);
   }
-}
+};
+
+document.querySelector("#fetch-button").addEventListener("click", function(){
+  weather.search();
+});
+
+document.querySelector("#search-input").addEventListener("keyup", function(event){
+  if(event.key == "Enter"){
+    weather.search();
+  }
+})
+
 
   
