5d1cc69667a7d02f7b16e1a0519d0b539e40a8f1
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-27 22:00:14 -0500

    Adding js script

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,12 +1,11 @@
-var APIKey = "46b2f5e4840b0ff2146a6e57e65a86d2";
+var apiKey = "46b2f5e4840b0ff2146a6e57e65a86d2";
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 var fetchButton = document.getElementById('fetch-button');
 var cityNameEl = document.getElementById('cityName');
 var tempEl = document.getElementById('temp');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
-var uvIndexEl = document.getElementById('uvIndex');
 // var dateEl = document.getElementById('')  <= Needs today's date displayed and 5 days out displayed as well
 
 var todayResultContentEl = document.querySelector('#today-result-content');
@@ -15,23 +14,14 @@ var searchFormEl = document.querySelector('#search-form');
 
 //Need to add local storage to store any persistent data
 
-
 var searchFormEl = document.querySelector('#search-form');
 
-function getApi() {
-  fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
-    });
+let weather = {
+  fetchWeather: function () {
+    fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  }
 }
-
-fetchButton.addEventListener('click', getApi);
 
   
